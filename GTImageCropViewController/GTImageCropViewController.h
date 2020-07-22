b12f5f7147f59644402f5c18925fec4b5893db77
t.kazama <takeo@kazama.biz>
2020-07-22 13:27:22 +0900

    Initial Commit

---
+++ GTImageCropViewController/GTImageCropViewController.h
@@ -0,0 +1,19 @@
+//
+//  GTImageCropViewController.h
+//  GTImageCropViewController
+//
+//  Created by 風間剛男 on 2020/07/22.
+//  Copyright © 2020 SHIJISHA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GTImageCropViewController.
+FOUNDATION_EXPORT double GTImageCropViewControllerVersionNumber;
+
+//! Project version string for GTImageCropViewController.
+FOUNDATION_EXPORT const unsigned char GTImageCropViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTImageCropViewController/PublicHeader.h>
+
+
